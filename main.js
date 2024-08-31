701aa50c75df6c6f357dcb5484b8369aae943944
bushraasabir <bushrasabir508@gmail.com>
2024-08-31 12:30:31 +0500

    push my currency converter to github

---
+++ main.js
@@ -0,0 +1,40 @@
+#!/usr/bin/env node
+import inquirer from "inquirer";
+console.log("\n \t WELCOME TO CURRENCY CONVERTER \n");
+const currency = {
+    //BASE CURRENCY
+    USD: 1,
+    EUR: 0.91,
+    GBP: 0.76,
+    INR: 74.57,
+    PKR: 280,
+    CAD: 1.35,
+    AUD: 1.48,
+    NZD: 1.60,
+    HKD: 7.80,
+};
+let user_answer = await inquirer.prompt([
+    {
+        name: "from",
+        message: "Enter from currrency",
+        type: "list",
+        choices: ["USD", "EUR", "GBP", "INR", "PKR", "CAD", "AUD", "NZD", "HKD"]
+    },
+    {
+        name: "to",
+        message: "Enter to currrency",
+        type: "list",
+        choices: ["USD", "EUR", "GBP", "INR", "PKR", "CAD", "AUD", "NZD", "HKD"]
+    },
+    {
+        name: "amount",
+        message: "Enter your amount",
+        type: "number",
+    }
+]);
+let fromAmount = currency[user_answer.from];
+let toAmount = currency[user_answer.to];
+let amount = user_answer.amount;
+let baseAmount = amount / fromAmount;
+let convertedAmount = baseAmount * toAmount;
+console.log(Math.round(convertedAmount));
